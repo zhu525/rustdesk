619609fce0512e68facb653dc95bf5de6dca8fe3
zhu525 <86352500+zhu525@users.noreply.github.com>
2025-08-09 21:49:14 +0800

    Update common.rs

--- src/common.rs
+++ src/common.rs
@@ -1004,7 +1004,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "http://rustdesk.liu66.dpdns.org:21114".to_owned()
+    "http://rustdesk.com:21114".to_owned()
 }
 
 #[inline]
